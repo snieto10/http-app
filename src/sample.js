34901985770d0946f4670ac03277c610d2386ac7
Sergio Nieto <snieto10@gmail.com>
2022-09-19 19:53:25 -0400

    promise exercise

---
+++ src/sample.js
@@ -0,0 +1,27 @@
+const userLeft = false;
+const userWatchingCatMeme = false;
+
+function watchTutorialCallback(callback, errorCallback) {
+  if (userLeft) {
+    errorCallback({
+      name: "User Left",
+      message: ":(",
+    });
+  } else if (userWatchingCatMeme) {
+    errorCallback({
+      name: "User Watching Cat Meme",
+      message: "WebDevSimplified < Cat",
+    });
+  } else {
+    callback("Thumbs up and Subscribe");
+  }
+}
+
+watchTutorialCallback(
+  (message) => {
+    console.log("Success: " + message);
+  },
+  (error) => {
+    console.log(error.name + " " + error.message);
+  }
+);
