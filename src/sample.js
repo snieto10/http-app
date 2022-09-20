8b8bf0d9290a6d4a0d34135c3752e9534e3a5893
Sergio Nieto <snieto10@gmail.com>
2022-09-19 21:29:24 -0400

    async and away

--- src/sample.js
+++ src/sample.js
@@ -1,27 +1,35 @@
-const userLeft = false;
-const userWatchingCatMeme = false;
-
-function watchTutorialCallback(callback, errorCallback) {
-  if (userLeft) {
-    errorCallback({
-      name: "User Left",
-      message: ":(",
-    });
-  } else if (userWatchingCatMeme) {
-    errorCallback({
-      name: "User Watching Cat Meme",
-      message: "WebDevSimplified < Cat",
-    });
-  } else {
-    callback("Thumbs up and Subscribe");
-  }
+function makeRequest(location) {
+  return new Promise((resolve, reject) => {
+    console.log(`Making Request to ${location}`);
+    if (location === "Google") {
+      resolve("Google says hi");
+    } else {
+      reject("We can only talk to Google");
+    }
+  });
 }
 
-watchTutorialCallback(
-  (message) => {
-    console.log("Success: " + message);
-  },
-  (error) => {
-    console.log(error.name + " " + error.message);
-  }
-);
+function processRequest(response) {
+  return new Promise((resolve, reject) => {
+    console.log("Processing response");
+    resolve(`Extra Information + ${response}`);
+  });
+}
+
+// makeRequest('Google').then(response => {
+//     console.log('Response Received')
+//     return processRequest(response)
+// }).then(processedResponse => {
+//     console.log(processedResponse)
+// }).catch(err => {
+//     console.log(err)
+// })
+
+function doWork() {
+  const response = makeRequest("Google");
+  console.log("Response Received");
+  const processedResponse = processRequest(response);
+  console.log(processedResponse);
+}
+
+doWork();
